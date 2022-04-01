c7719b21f981768e790cb2b51fdd0eba6e426a07
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-03-31 19:43:37 -0500

    adding some java code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
  //add your javascrip between these two lines of code
-  
+  var d1 = document.getElementById('entryinput');
+  alert(d1.value);
+  d1.addEventListener('onclick', myEventFunction);
  
  
  
  
  
  
- 
+}
  window.addEventListener('load', init);
