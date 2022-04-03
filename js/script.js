3eea14dd4ea7523c70bc7d5c054fed290e6e633f
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-03 18:56:51 -0500

    adding name to alert

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ function init(){
   var d1 = document.getElementById('entryinput');
   var d2 = document.getElementById('entrybutton');
   var d3 = document.getElementById('textoutput');
+  var name = document.getElementsByTagName("h2");
   d2.addEventListener('click', function(){
-    alert(d1.value);
+    alert(name.value + ":" + d1.value);
     d3.innerHTML = d1.value;
   });
 }
