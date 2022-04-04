7627707c195a8a99be0e1f11eb97f10dd3a9a5b1
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-03 19:09:13 -0500

    fixing error

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function init(){
   var d1 = document.getElementById('entryinput');
   var d2 = document.getElementById('entrybutton');
   var d3 = document.getElementById('textoutput');
-  var name = document.getElementsById("Na");
+  var name = document.getElementById("Na");
   d2.addEventListener('click', function(){
     alert(name.value + ": " + d1.value);
     d3.innerHTML = d1.value;
